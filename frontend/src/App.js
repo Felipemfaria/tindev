0f02aca19acc298adc42cd3426d16d57132539d7
Felipe <felipemfaria@outlook.com>
2020-04-14 23:37:53 -0300

    Created logo screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Login from './pages/Login'
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Login />
   );
 }
 
 export default App;
+ 
